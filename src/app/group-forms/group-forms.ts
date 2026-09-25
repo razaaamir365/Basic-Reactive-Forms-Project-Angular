df8961a2f045ff006a4186d390b0821b400a734b
razaaamir365 <razaaamir365@gmail.com>
2026-09-25 12:49:20 +0530

    add reactive forms validation

--- src/app/group-forms/group-forms.ts
+++ src/app/group-forms/group-forms.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-group-forms',
@@ -9,11 +9,26 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class GroupForms {
   loginForm=new FormGroup({
-    firstName:new FormControl(''),
-    lastName:new FormControl(''),
-    email:new FormControl(''),
-    password:new FormControl(''),
+    firstName:new FormControl('',[Validators.required,Validators.minLength(2),Validators.maxLength(20)]),
+    lastName:new FormControl('',[Validators.required,Validators.minLength(2),Validators.maxLength(20)]),
+    email:new FormControl('',[Validators.required,Validators.email]),
+    password:new FormControl('',[Validators.required,Validators.minLength(5)]),
   })
+
+ get firstName(){
+    return this.loginForm.get("firstName")
+  }  
+  get lastName(){
+    return this.loginForm.get("lastName")
+  }   
+  get email(){
+    return this.loginForm.get("email")
+  }
+    get password(){
+    return this.loginForm.get("password")
+  }
+
+
   handleProfile(){
     console.log(this.loginForm.value);
   }
